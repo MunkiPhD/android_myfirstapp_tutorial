4e34498c32a4703f183b5ca5a7c76b9f42a525eb
MunkiPhD <munkiphd@gmail.com>
2014-10-13 18:14:56 -0400

    added second activity

---
+++ app/src/main/java/com/example/menace/myfirstapp/DisplayMessageActivity.java
@@ -0,0 +1,43 @@
+package com.example.menace.myfirstapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class DisplayMessageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MyActivity.EXTRA__MESSAGE);
+
+        // and now add a textview
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        // set teh text view as the activity layout
+        setContentView(textView);
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/example/menace/myfirstapp/MyActivity.java
+++ app/src/main/java/com/example/menace/myfirstapp/MyActivity.java
@@ -1,12 +1,25 @@
 package com.example.menace.myfirstapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MyActivity extends Activity {
+    public final static String EXTRA__MESSAGE = "com.example.menace.myfirstapp.MESSAGE";
+
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText)findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA__MESSAGE, message);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
